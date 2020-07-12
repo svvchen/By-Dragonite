ed6526fe012327302f44b307e1f6274cc87bef88
svvchen <svvchen@gmail.com>
2020-07-12 12:42:04 -0700

    Updated settings

--- dragonite_newsletter/settings.py
+++ dragonite_newsletter/settings.py
@@ -20,12 +20,15 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '+*l)vue7c)4py9(i8de5nt)iz^tahdm(q7@a9&ypo=irp*p!1t'
+
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    'boiling-river-15270.herokuapp.com'
+]
 
 
 # Application definition
